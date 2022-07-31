89a8b2ecf498717bc1bd6a09791e3a7904d14bdf
Ryoengineer112358 <ryoengineer112358@gmail.com>
2022-07-31 13:40:23 +0900

    Controllerにて投稿の一覧を取得し、ページに表示

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,13 +3,18 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Post;
 
 class PostController extends Controller
 {
     public function index()
     {
+        // $posts = Post::all();
+        // $posts = Post::orderBy('created_at', 'desc')->get();
+        $posts = Post::latest()->get();
+
         return view('index')
-            ->with(['posts' => $this->posts]);
+            ->with(['posts' => $posts]);
     }
 
     // idを引数として渡す
