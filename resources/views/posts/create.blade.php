a4327f998613d59ee3cf5c48613500a3be2a38a9
Ryoengineer112358 <ryoengineer112358@gmail.com>
2022-08-01 13:20:51 +0900

    Viewに投稿フォームを追加, CSRF対策の実装

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -7,4 +7,18 @@
         &laquo; <a href="{{ route('posts.index') }}">Back</a>
     </div>
     <h1>Add New Post - My BBS</h1>
+
+    <form method="post" action="">
+        @csrf
+
+        <label>
+            Title
+            <input type="text" name="title">
+        </label>
+        <label>
+            Body
+            <textarea name="body"></textarea>
+        </label>
+        <button>Add</button>
+    </form>
 </x-layout>
