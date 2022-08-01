924fde84883eea8039e7601a21ddc5dc9cbcd8c2
Ryoengineer112358 <ryoengineer112358@gmail.com>
2022-08-01 13:32:22 +0900

    投稿フォームのスタイリング

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -6,19 +6,26 @@
     <div class="back-link">
         &laquo; <a href="{{ route('posts.index') }}">Back</a>
     </div>
-    <h1>Add New Post - My BBS</h1>
+
+    <h1>Add New Post</h1>
 
     <form method="post" action="">
         @csrf
 
-        <label>
-            Title
-            <input type="text" name="title">
-        </label>
-        <label>
-            Body
-            <textarea name="body"></textarea>
-        </label>
-        <button>Add</button>
+        <div class="form-group">
+            <label>
+                Title
+                <input type="text" name="title">
+            </label>
+        </div>
+        <div class="form-group">
+            <label>
+                Body
+                <textarea name="body"></textarea>
+            </label>
+        </div>
+        <div class="form-button">
+            <button>Add</button>
+        </div>
     </form>
 </x-layout>
